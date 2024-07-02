2602ffeb794e152984dd940f8e2081c3d4169ed3
minju <ngit0001@gmail.com>
2024-07-02 10:34:04 +0900

    blog 페이지나누기 완료

--- django/blog/blog/models.py
+++ django/blog/blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 
 class Post(models.Model):
@@ -9,6 +10,7 @@ class Post(models.Model):
     image = models.ImageField(blank=True, null=True, upload_to="image")
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
+    tags = TaggableManager()
 
     # auto_now_add : 처음에만 날짜 입력
     # auto_now : 수정될 때마다 날짜 입력
